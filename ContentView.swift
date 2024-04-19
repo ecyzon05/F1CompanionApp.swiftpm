5581ec8c0c0b337c596ecb6509eecb64852d4f25
Ryder <rdelacruz4092@stu.d214.org>
2024-04-19 12:53:01 -0500

    Created 3 text boxes that are conformed to be buttons

--- ContentView.swift
+++ ContentView.swift
@@ -2,11 +2,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            Button("F1 Teams") {
+                
+                print("Make this go to the teams tab")
+            }
+            
+            Button ("Racetracks") {
+                
+                print("Make this go to the racetracks tab")
+            }
+            
+            Button("Championships Standings") {
+                
+                print ("make this got the championships standings tab")
+            }
+            
+            
+            
+            
         }
     }
 }
