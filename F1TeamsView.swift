3de6b292ac8cd2bdd51fb7572acef62c63d38022
Practice1 <tkolomiiets4218@stu.d214.org>
2024-04-19 13:29:18 -0500

    So far, added 3 team logos that will be buttons later on.

--- F1TeamsView.swift
+++ F1TeamsView.swift
@@ -1,1 +1,20 @@
 import SwiftUI
+
+struct F1TeamsView: View {
+    var body: some View {
+        VStack {
+            Button {
+            } label: {
+                Image(systemName: "MercedesF1")
+            }
+            Button {
+            } label: {
+                Image(systemName: "FerrariF1")
+            }
+            Button {
+            } label: {
+                Image(systemName: "RedBullF1")
+            }
+        }
+    }
+}
