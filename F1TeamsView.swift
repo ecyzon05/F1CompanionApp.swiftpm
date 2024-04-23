e7cb5daca586a800c5f914934ae3c0eb0e6abc29
Practice1 <tkolomiiets4218@stu.d214.org>
2024-04-23 13:23:40 -0500

    Added 10 team logos, all will function as buttons

--- F1TeamsView.swift
+++ F1TeamsView.swift
@@ -2,18 +2,84 @@ import SwiftUI
 
 struct F1TeamsView: View {
     var body: some View {
-        VStack {
-            Button {
-            } label: {
-                Image(systemName: "MercedesF1")
+        HStack(spacing: 5) {
+            VStack(spacing: 5) {
+                Button(action: {
+                }) {
+                    Image("MercedesAMGF1")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 150)
+                    
+                }
+                
+                Button(action: {
+                }) {
+                    Image("FerrariF1")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 200)
+                }
+                
+                Button(action: {
+                }) {
+                    Image("RedBullF1")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 350, height: 150)
+                }
+                
+                Button(action: {
+                }) {
+                    Image("McLarenF1")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 150)
+                }
+                Button(action: {
+                }) {
+                    Image("AstonMartinF1")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 100)
+                }
             }
-            Button {
-            } label: {
-                Image(systemName: "FerrariF1")
+            VStack {
+                Button(action: {
+                }) {
+                    Image("VisaCashAppRBF1")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 400, height: 250)
             }
-            Button {
-            } label: {
-                Image(systemName: "RedBullF1")
+                Button(action: {
+                }) {
+                    Image("HaasF1")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 400, height: 200)
+                }
+                Button(action: {
+                }) {
+                    Image("WilliamsF1")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 100)
+                }
+                Button(action: {
+                }) {
+                    Image("StakeSauberF1")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 100)
+                }
+                Button(action: {
+                }) {
+                    Image("AlpineF1")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 100)
+                }
             }
         }
     }
