e5766ce2b90f8b448f869846ed43418be0fe3764
Ethan Cyzon <ecyzon4088@gmail.com>
2024-04-23 13:01:50 -0500

    Added navigation views

---
+++ StandingsView.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+struct ChampionshipStandingsView: View {
+    var body: some View {
+        Text("Championship Standings Content Goes Here")
+    }
+}
+
---
+++ TrackView.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+struct TrackView: View {
+    var body: some View {
+        Text("Championship Standings Content Goes Here")
+    }
+}
+
