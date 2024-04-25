bd372bd66f3f0dc58b5b7c6cb3f5cd671e77a03a
Ryder <rdelacruz4092@stu.d214.org>
2024-04-25 13:28:52 -0500

    Added Austria

---
+++ Austria.swift
@@ -0,0 +1,25 @@
+//
+//  Austria.swift
+//  F1CompanionApp
+//
+//  Created by Ryder De La Cruz on 4/25/24.
+//
+
+import SwiftUI
+
+struct AustriaView: View {
+    
+    @Binding var Form: Information
+    
+    var body: some View {
+        NavigationStack{
+            VStack{
+                NavigationLink("Back to home page") {
+                    
+                    ContentView(Form: $Form)
+                }
+            }
+        }
+    }
+    
+}
