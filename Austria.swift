ef98b9bdf211e5c8e169c8a3d22424526363e5b7
Practice1 <tkolomiiets4218@stu.d214.org>
2024-04-29 12:39:30 -0500

    FIXING

--- Austria.swift
+++ Austria.swift
@@ -2,19 +2,43 @@
 
 import SwiftUI
 
-struct AustriaView: View {
+struct ChampionshipStandingsView: View {
     
     @Binding var Form: Information
     
     var body: some View {
         NavigationStack{
-            VStack{
-                NavigationLink("Back to home page") {
-        
-                    ContentView(Form: $Form)
+            HStack {
+                VStack {
+                    Button(action: {
+                    }) {
+                        Image("WDCTrophy")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 400, height: 500)
+                    }
+                    Text("World Drivers Championship Stadings")
+                }
+                VStack {
+                    Button(action: {
+                    }) {
+                        Image("WCCTrophy")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 370, height: 440)
+                    }
+                    Text("World Constructors Championship Stadings")
+                }
+            }
+            NavigationStack{
+                VStack{
+                    NavigationLink("Back to home page") {
+                        
+                        ContentView(Form: $Form)
+                    }
                 }
             }
         }
     }
-    
 }
+    
