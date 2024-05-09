5597b007817a1970e1b2c06edc9ed570c3a54a96
Ryder <rdelacruz4092@stu.d214.org>
2024-05-09 12:48:43 -0500

    home

--- AlpineView.swift
+++ AlpineView.swift
@@ -6,8 +6,25 @@ struct AlpineView: View {
 
 
     @Binding var Form: Information
-    
+     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     var body: some View {
+        Spacer()
+            .navigationBarBackButtonHidden(true)
+            .toolbar(content: {
+                ToolbarItem (placement: .navigationBarLeading) {
+                    
+                    Button(action: {
+                        presentationMode
+                            .wrappedValue
+                            .dismiss()
+                    }, label: {
+                        Image(systemName: "house")
+                            .foregroundColor(.blue)
+                        Text("Home")
+                            .foregroundColor(.blue)
+                    })
+                }
+            })
             VStack{
                 NavigationLink("Go to Alpine Team!") {
                     
--- AstonMartinView.swift
+++ AstonMartinView.swift
@@ -6,8 +6,25 @@ struct AstonMartinView: View {
    
 
     @Binding var Form: Information
-    
+     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     var body: some View {
+        Spacer()
+            .navigationBarBackButtonHidden(true)
+            .toolbar(content: {
+                ToolbarItem (placement: .navigationBarLeading) {
+                    
+                    Button(action: {
+                        presentationMode
+                            .wrappedValue
+                            .dismiss()
+                    }, label: {
+                        Image(systemName: "house")
+                            .foregroundColor(.blue)
+                        Text("Home")
+                            .foregroundColor(.blue)
+                    })
+                }
+            })
             VStack{
                 NavigationLink("Go to Aston's Team!") {
                     
